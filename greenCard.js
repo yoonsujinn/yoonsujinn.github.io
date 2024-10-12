8a44fdabb0d72fb5e6f4a3bc6730d1786eecbfa9
yoonsujinn <yoonsujin2@naver.com>
2024-10-12 19:10:06 +0900

    aa

--- greenCard.js
+++ greenCard.js
@@ -1,25 +1,38 @@
 
 
-    let imgArr = [
-        'https://www.green-card.co.kr/pc/assets/images/bg/bg-main-visual-bg-01.png',
-        'https://www.green-card.co.kr/pc/assets/images/bg/bg-main-visual-bg-02.png',
-        'https://www.green-card.co.kr/pc/assets/images/bg/bg-main-visual-bg-03.png',
-    ]
-    let imgArrindex = 0;
-    const leftMoveBg = document.querySelector('.leftMoveBg')
-    setInterval(function(){
-        if(imgArrindex <= imgArr.length) {
-            console.log (imgArrindex-1)
-            imgArrindex ++;
-        }else if(imgArrindex >= imgArr.length) {
-        console.log (imgArrindex-1)
-        imgArrindex = 0;
 
-        }
-    },1000)
+        let imgArr1 = [
+            './images/bg-main-visual-bg-01.png',
+            './images/bg-main-visual-bg-02.png',
+            './images/bg-main-visual-bg-03.png',
+        ]
+        let imgArr2 = [
+            './images/bg-main-visual-bg-01.png',
+            './images/bg-main-visual-bg-02.png',
+            './images/bg-main-visual-bg-03.png',
+        ]
+        
+        const leftMoveBg = document.querySelector('.leftMoveBg')
+        let imgArrindex = 0;
+
+    
+   
+    // setInterval(function(){
+
+    //     if(imgArrindex < imgArr1.length) {
+    //         leftMoveBg.style.backgroundImage = `url(${imgArr1[imgArrindex]})`;
+    //         leftMoveBg.style.opacity = '1'
+    //         imgArrindex ++;
+    //     }else{
+    //         imgArrindex = 0;
+    //         leftMoveBg.style.backgroundImage = `url(${imgArr1[imgArrindex]})`;
+    //         leftMoveBg.style.opacity = '1'
+
+    //         imgArrindex ++;
+    //     }
+    // },3000)
 
 
         
 
-        // leftMoveBg.style.backgroundImage = "url('https://www.green-card.co.kr/pc/assets/images/bg/bg-main-visual-bg-01.png')";
 
