33ffe6a0debedc571987e89509828c30ae9173ce
yoonsujinn <yoonsujin2@naver.com>
2024-10-13 13:24:18 +0900

    zzz

--- greenCard.js
+++ greenCard.js
@@ -1,36 +1,30 @@
 
 
 
-        let imgArr1 = [
-            './images/bg-main-visual-bg-01.png',
-            './images/bg-main-visual-bg-02.png',
-            './images/bg-main-visual-bg-03.png',
-        ]
-        let imgArr2 = [
-            './images/bg-main-visual-bg-01.png',
-            './images/bg-main-visual-bg-02.png',
-            './images/bg-main-visual-bg-03.png',
-        ]
-        
-        const leftMoveBg = document.querySelector('.leftMoveBg')
-        let imgArrindex = 0;
+let imgArr1 = [
+    './images/bg-main-visual-bg-01.png',
+    './images/bg-main-visual-bg-02.png',
+    './images/bg-main-visual-bg-03.png',
+]
+const leftMoveBg = document.querySelector('.leftMoveBg')
+let imgArrindex = 0;
 
-    
-   
-    // setInterval(function(){
 
-    //     if(imgArrindex < imgArr1.length) {
-    //         leftMoveBg.style.backgroundImage = `url(${imgArr1[imgArrindex]})`;
-    //         leftMoveBg.style.opacity = '1'
-    //         imgArrindex ++;
-    //     }else{
-    //         imgArrindex = 0;
-    //         leftMoveBg.style.backgroundImage = `url(${imgArr1[imgArrindex]})`;
-    //         leftMoveBg.style.opacity = '1'
-
-    //         imgArrindex ++;
-    //     }
-    // },3000)
+// section1 - 캐릭터 4초에 한번씩 변경되는 스크립트
+    setInterval(function(){
+        if(imgArrindex < imgArr1.length) {
+            leftMoveBg.style.backgroundImage = `url(${imgArr1[imgArrindex]})`;
+            leftMoveBg.classList.remove('leftMoveBg_1','leftMoveBg_2','leftMoveBg_3')
+            leftMoveBg.classList.add(`leftMoveBg_${imgArrindex + 1}`)
+            imgArrindex ++;
+        }else{
+            imgArrindex = 0;
+            leftMoveBg.style.backgroundImage = `url(${imgArr1[imgArrindex]})`;
+            leftMoveBg.classList.remove('leftMoveBg_1','leftMoveBg_2','leftMoveBg_3')
+            leftMoveBg.classList.add(`leftMoveBg_${imgArrindex + 1}`)
+            imgArrindex ++;
+        }
+    },4000)
 
 
         
