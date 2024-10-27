7cf4871d18c7ab591eb6d58d816518e9bd404ba7
yoonsujinn <yoonsujin2@naver.com>
2024-10-27 15:42:16 +0900

    아아

--- greenCard.js
+++ greenCard.js
@@ -8,21 +8,18 @@ let imgArr1 = [
 ]
 const leftMoveBg = document.querySelector('.leftMoveBg')
 const rightTitle =  document.querySelectorAll('.rightTitle')
-let imgArrindex = 0;
-let scrolling;
-const scWidth = window.innerWidth;
-const scHeight = window.innerHeight;
+let scWidth = window.innerWidth;
+let scHeight = window.innerHeight;
 window.addEventListener("resize",updateScreenSize)
-
 
 
 function updateScreenSize() {
     scWidth = window.innerWidth;
     scHeight = window.innerHeight;
 }
-
 // section1 - 캐릭터 4초에 한번씩 변경되는 스크립트
     setInterval(function(){
+        let imgArrinde
         if(imgArrindex < imgArr1.length) {
             leftMoveBg.style.backgroundImage = `url(${imgArr1[imgArrindex]})`;
             leftMoveBg.classList.remove('leftMoveBg_1','leftMoveBg_2','leftMoveBg_3')
@@ -37,46 +34,35 @@ function updateScreenSize() {
         }
     },4000)
 
-
         window.addEventListener("wheel", function(event) {
             let scrollXY = event.deltaY; //스크롤 방향 확인
-            let scrollPosition = window.scrollY; // 스크롤 위치 확인
+            let scrollPosition = window.scrollY; // 스크롤 현재 위치 확인
+            console.log(scrollXY);
                 setTimeout(() => {
-                if(scrollXY > 0 ) {
-                    if(scrollPosition <= 700) {
+                if(scrollXY > 0 ) { //내려갈때
+                    if(scrollPosition <= scHeight) {
                         window.scroll({
                         top: scHeight*1, 
                         behavior: 'smooth' 
                         });
-                    }else if(scrollPosition <= 1400) {
+                    }
+                    else if(scrollPosition <= scHeight*2) {
                         window.scroll({
                             top: scHeight*2, 
                             behavior: 'smooth' 
                         });
-                    }else if(scrollPosition <= 2100) {
+                    }else if(scrollPosition <= scHeight*3) {
                         window.scroll({
                             top: scHeight*3, 
                             behavior: 'smooth' 
                         });
                     }
-                }
-                 g
-                });  
-            }, 1000);
-    
-    // window.addEventListener("scroll",function() {
-    //     let ScrollPosition = window.scrollY; // 스크롤 위치 확인
-    //     rightTitle.forEach(e => {
-    //         if(ScrollPosition >= 0 ||ScrollPosition <= 500) {
-    //         e.classList.add('on')
-    //         }else {
-    //             e.classList.remove('on')
-    //         }
-                
-    //         });
-        
+                }else if( scrollXY < 50) { //올라갈때
 
-    // })
+                }
+                });  
+            }, 500);
+    
 
 
 
