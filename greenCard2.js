f08091c57a488b4b2dee33d2e5907da26424f74e
yoonsujinn <yoonsujin2@naver.com>
2024-12-06 12:00:46 +0900

    dsadas

--- greenCard2.js
+++ greenCard2.js
@@ -1,19 +1,23 @@
 
 let contTopMenuLi = document.querySelectorAll('.contTopMenu li');
 let custContent_1 = document.querySelectorAll('.custContent_1');
-let custContent_2= document.querySelectorAll('.custContent_2');
+let custContent_2 = document.querySelectorAll('.custContent_2');
 
 contTopMenuLi.forEach(element => {
-    element.addEventListener('click',function(e){
+    element.addEventListener('click', function (e) {
         contTopMenuLi[0].classList.remove('on')
         contTopMenuLi[1].classList.remove('on')
         element.classList.add('on')
-
-        if(contTopMenuLi[0].classList.contains('on')) {
+        if (contTopMenuLi[0].classList.contains('on')) {
             console.log('11111111111')
-        }else if(contTopMenuLi[1].classList.contains('on')) {
+            console.log(custContent_2)
+
+            custContent_2.classList.remove('on')
+            custContent_1.classList.add('on')
+        } else if (contTopMenuLi[1].classList.contains('on')) {
             console.log('2222222222')
 
-    }
+
+        }
     })
 });
